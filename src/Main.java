60abc5d61bf498fa00498866327c6cf2f640f2c8
Lucas Venezian Povoa <lucasvenez@gmail.com>
2025-09-12 09:04:27 -0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(String ... args) {
         int a = 15;
-        System.out.println("Olá, Mundo! " + String.valueOf(a));
+        System.out.println("Olá, Mundo asdasda! " + String.valueOf(a));
     }
 }
